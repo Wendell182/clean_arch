64105935ec75442d931e34a419aa497b8a05e10b
user <user@MB033728.local>
2022-01-31 15:44:25 -0300

    ensure HttpAdapter throws NotFoundError  on 404

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -130,6 +130,13 @@ void main() {
       expect(future, throwsA(HttpError.forbidden));
     });
 
+    test('should return ForbiddenError if post returns 404', () async {
+      mockResponse(404);
+
+      final future = sut.request(url: url, method: 'post');
+      expect(future, throwsA(HttpError.notFound));
+    });
+
     test('should return Servererror if post returns 500', () async {
       mockResponse(500);
 
