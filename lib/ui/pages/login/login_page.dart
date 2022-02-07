85a51277222d5653377928045aa7d6a8db9f09a4
user <user@MB033728.local>
2022-02-07 13:06:46 -0300

    move LoginButton to its own file

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -57,16 +57,7 @@ class _LoginPageState extends State<LoginPage> {
                         padding: EdgeInsets.only(top: 8.0, bottom: 32.0),
                         child: PasswordInput(),
                       ),
-                      StreamBuilder<bool>(
-                          stream: widget.presenter.isFormValidStream,
-                          builder: (context, snapshot) {
-                            return ElevatedButton(
-                              onPressed: snapshot.data == true
-                                  ? widget.presenter.auth
-                                  : null,
-                              child: Text('Entrar'),
-                            );
-                          }),
+                      LoginButton(),
                       TextButton(
                         onPressed: () {},
                         child: Row(
