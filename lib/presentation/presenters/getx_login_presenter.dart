089c37e053ac9a656d5ed9b71a9fb62a43cb1090
Wendell Claus Leite <wendellclaus@brq.com>
2022-03-23 22:24:37 -0300

    refactory LoginPresenter with Getx

---
+++ lib/presentation/presenters/getx_login_presenter.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:get/state_manager.dart';
+import 'package:manga_clean_arch/domain/helpers/helpers.dart';
+import 'package:manga_clean_arch/ui/pages/pages.dart';
+
+import '../../domain/usecases/usecases.dart';
+import '../protocols/protocols.dart';
+
+class GetxLoginPresenter extends GetxController implements LoginPresenter {
+  final Validation validation;
+  final Authentication authentication;
+
+  String _email;
+  String _password;
+  var _emailError = Rxn<String>();
+  var _passwordError = Rxn<String>();
+  var _mainError = Rxn<String>();
+  var _isFormValid = false.obs;
+  var _isLoading = false.obs;
+
+  @override
+  Stream<String> get emailErrorStream => _emailError.stream;
+
+  @override
+  Stream<String> get passwordErrorStream => _passwordError.stream;
+
+  @override
+  Stream<String> get mainErrorStream => _mainError.stream;
+
+  @override
+  Stream<bool> get isFormValidStream => _isFormValid.stream;
+
+  @override
+  Stream<bool> get isLoadingStream => _isLoading.stream;
+
+  GetxLoginPresenter(
+      {@required this.validation, @required this.authentication});
+
+  void validateEmail(String email) {
+    _email = email;
+    _emailError.value = validation.validate(field: 'email', value: email);
+    _validateForm();
+  }
+
+  void validatePassword(String password) {
+    _password = password;
+    _passwordError.value =
+        validation.validate(field: 'password', value: password);
+    _validateForm();
+  }
+
+  void _validateForm() {
+    _isFormValid.value = _emailError.value == null &&
+        _passwordError.value == null &&
+        _email != null &&
+        _password != null;
+  }
+
+  Future<void> auth() async {
+    _isLoading.value = true;
+    try {
+      await authentication
+          .auth(AuthenticationParams(email: _email, secret: _password));
+    } on DomainError catch (error) {
+      _mainError.value = error.description;
+    }
+    _isLoading.value = false;
+  }
+
+  void dispose() {}
+}
