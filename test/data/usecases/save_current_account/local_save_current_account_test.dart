2dcb8e9e8eb0cc2a2a06e15ce785c7477350b769
Wendell Claus Leite <wendellclaus@brq.com>
2022-04-24 16:04:13 -0300

    move prod code to their own files

--- test/data/usecases/save_current_account/local_save_current_account_test.dart
+++ test/data/usecases/save_current_account/local_save_current_account_test.dart
@@ -1,29 +1,10 @@
 import 'package:faker/faker.dart';
+import 'package:manga_clean_arch/data/cache/save_secure_cache_storage.dart';
+import 'package:manga_clean_arch/data/usecases/save_current_account/local_save_current_account.dart';
 import 'package:manga_clean_arch/domain/helpers/helpers.dart';
-import 'package:meta/meta.dart';
 import 'package:manga_clean_arch/domain/entities/entities.dart';
-import 'package:manga_clean_arch/domain/usecases/save_currente_account.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-
-class LocalSaveCurrentAccount implements SaveCurrentAccount {
-  SaveSecureCachStorage saveSecureCachStorage;
-
-  LocalSaveCurrentAccount({@required this.saveSecureCachStorage});
-
-  Future<void> save(AccountEntity account) async {
-    try {
-      await saveSecureCachStorage.saveSecure(
-          key: 'token', value: account.token);
-    } catch (error) {
-      throw DomainError.unexpected;
-    }
-  }
-}
-
-abstract class SaveSecureCachStorage {
-  Future<void> saveSecure({@required String key, @required String value});
-}
 
 class SaveSecureCacheStorageSpy extends Mock implements SaveSecureCachStorage {}
 
